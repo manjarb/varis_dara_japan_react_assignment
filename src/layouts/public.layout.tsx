89ab53f8fec72587e6236410485e06d90086d015
Varis Darasirikul <manjarb4@gmail.com>
2023-01-26 02:36:24 +0800

    feature/step-1: Add tailwind css

--- src/layouts/public.layout.tsx
+++ src/layouts/public.layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function PublicLayout() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Outlet />
     </div>
   );
